d8564934e628ba43ae169820e0e69192c23f879a
Tomás Rossi <91207352+rossitom90@users.noreply.github.com>
2022-10-05 19:17:34 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg'
+import './App.css'
+import NavBar from './components/NavBar'
+import Clicker from './components/clicker'
+import ItemList from './components/shop/ItemList'
+import ItemDetail from './components/shop/ItemDetails'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Error404 from './components/Error404'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <NavBar />
+
+        <Routes>
+          <Route path={'/'} element={<Clicker />} />
+          <Route path={'/shop'} element={<ItemList />} />
+          <Route path={'/shop/item/:id'} element={<ItemDetail />} />
+          <Route path={'/*'} element={<Error404 />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
